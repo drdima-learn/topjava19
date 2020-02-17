72b5f55a82c239ca2f8a27ff712b3aec58f4e12e
drdima2 <drdima2@gmail.com>
2020-02-17 00:57:20 -0600

    meals add and delete done

--- src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
 import ru.javawebinar.topjava.web.user.AbstractUserController;
 
 import java.time.LocalDateTime;
@@ -17,23 +18,23 @@ import java.util.List;
 @RequestMapping("/ajax/meals")
 public class MealUIController extends AbstractMealController {
 
-//    @Override
-//    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//    public List<User> getAll() {
-//        return super.getAll();
-//    }
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
 
-//    @Override
-//    @DeleteMapping("/{id}")
-//    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-//    public void delete(@PathVariable int id) {
-//        super.delete(id);
-//    }
+    @Override
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void createOrUpdate(@RequestParam Integer id,
-                               @RequestParam @DateTimeFormat LocalDateTime dateTime,
+                               @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateTime,
                                @RequestParam String description,
                                @RequestParam Integer calories) {
 
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -65,8 +65,8 @@
 								</td>
 								<td>${meal.description}</td>
 								<td>${meal.calories}</td>
-								<td><a href="meals/update?id=${meal.id}"><spring:message code="common.update"/></a></td>
-								<td><a href="meals/delete?id=${meal.id}"><spring:message code="common.delete"/></a></td>
+								<td><a><span class="fa fa-pencil" alt="<spring:message code="common.update"/>"></span></a></td>
+								<td><a class="delete" id="${meal.id}" alt="<spring:message code="common.delete"/>"><span class="fa fa-remove" ></span></a></td>
 							</tr>
 						</c:forEach>
 					</table>
